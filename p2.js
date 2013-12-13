77b32170de675ae2f39d3d293a5084af069e831a
ebbflowgo <christopher.dolinski@gmail.com>
2013-12-13 02:23:19 -0500

    Create p2.js

---
+++ p2.js
@@ -0,0 +1,18 @@
+var a,b,result;
+
+a =0;
+b =1;
+sum = 0;
+result = 0;
+
+while(result < 4000000) {
+  console.log(result);
+  result = a+b;
+  a=b;
+  b=result;
+  if (result % 2 === 0) {
+    sum = sum+ result;
+  }
+}
+
+console.log(sum);
